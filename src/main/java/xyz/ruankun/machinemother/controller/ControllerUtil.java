ca9f37e0ff2257673de393bbf3a2628d1c9c0a1e
mayberk <qkmc@outlook.com>
2019-06-14 23:41:54 +0800

    金融模块完成一大半

---
+++ src/main/java/xyz/ruankun/machinemother/controller/ControllerUtil.java
@@ -0,0 +1,4 @@
+package xyz.ruankun.machinemother.controller;
+
+public class ControllerUtil {
+}
---
+++ src/main/java/xyz/ruankun/machinemother/service/impl/FinancialServiceImpl.java
@@ -0,0 +1,4 @@
+package xyz.ruankun.machinemother.service.impl;
+
+public class FinancialServiceImpl {
+}

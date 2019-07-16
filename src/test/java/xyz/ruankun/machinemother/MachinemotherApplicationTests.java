767663597c70cb0db6835217a78267bead0bb2e0
Jason <Jasonneeed@163.com>
2019-07-16 09:12:24 +0800

    取消order补丁

--- src/test/java/xyz/ruankun/machinemother/MachinemotherApplicationTests.java
+++ src/test/java/xyz/ruankun/machinemother/MachinemotherApplicationTests.java
@@ -16,13 +16,7 @@ public class MachinemotherApplicationTests {
 
 	@Test
 	public void teee(){
-		String str = MD5Util.md5("Jason");
-		System.out.println(str);
-		if(str .equals("472D46CB829018F9DBD65FB8479A49BB")){
-			System.out.println("equal");
-		}else{
-			System.out.println("not equal");
-		}
+		String str = MD5Util.trueMd5(12+"limo"+232.32);
 	}
 
 }
